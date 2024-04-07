7b0d25fb35dc1fdc8ef3f351e995ed8b5fc59047
Juan Andrés <juan.andresgiraldo@hotmail.com>
2024-04-06 19:07:02 -0500

    cambio index controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,8 @@
-export * from './ping.controller';
-export * from './estudiante-controller.controller';
-export * from './estudiante-apoyo-controller.controller';
 export * from './apoyo-socioeconomico.controller';
-export * from './resultado-convocatoria.controller';
-export * from './convocatoria.controller';
-export * from './estudiante-apoyo-socioeconomico.controller';
 export * from './convocatoria-apoyo-socioeconomico.controller';
+export * from './convocatoria.controller';
+export * from './estudiante-apoyo-controller.controller';
+export * from './estudiante-controller.controller';
+export * from './ping.controller';
+export * from './resultado-convocatoria.controller';
+
