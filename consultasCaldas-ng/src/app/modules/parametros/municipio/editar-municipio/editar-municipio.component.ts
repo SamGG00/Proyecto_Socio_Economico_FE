5f7c43e249dc15fcdf3d53be451bddb4326e1233
JuanGGarcia <juan1701829004@ucaldas.edu.co>
2024-06-10 16:15:44 -0500

    organizacion editar full

--- consultasCaldas-ng/src/app/modules/parametros/municipio/editar-municipio/editar-municipio.component.ts
+++ consultasCaldas-ng/src/app/modules/parametros/municipio/editar-municipio/editar-municipio.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfigurationData } from 'src/app/config/ConfigurationData';
+import { MunicipioModel } from 'src/app/models/parametros/municipio.model';
+import { MunicipioService } from 'src/app/services/parametros/municipio.service';
+declare const ShowGeneralMessage:any;
 @Component({
   selector: 'app-editar-municipio',
   templateUrl: './editar-municipio.component.html',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarMunicipioComponent implements OnInit {
 
-  constructor() { }
+  dataForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private service: MunicipioService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.FormBuilding();
+    this.SearchRecord();
+  }
+
+  FormBuilding() {
+    this.dataForm = this.fb.group({
+      id: ["", [Validators.required]],
+      Nombre: ["", [Validators.required]]
+    });
+  }
+
+  get GetDF() {
+    return this.dataForm.controls;
+  }
+
+  SearchRecord() {
+    let id = this.route.snapshot.params["id"];
+    this.service.SearchRecord(id).subscribe({
+      next: (data: MunicipioModel) => {
+        this.GetDF["id"].setValue(data.id);
+        this.GetDF["Nombre"].setValue(data.Nombre);
+      }
+    });
+  }
+
+  SaveRecord() {
+    let model = new MunicipioModel();
+    model.id = this.GetDF["id"].value;
+    model.Nombre = this.GetDF["Nombre"].value;
+
+    this.service.EditRecord(model).subscribe({
+      next: (data: MunicipioModel) => {
+        ShowGeneralMessage(ConfigurationData.UPDATED_MESSAGE);
+        this.router.navigate(["/parametros/listar-municipio"]);
+      },
+      error: (err) => {
+        ShowGeneralMessage(ConfigurationData.INVALID_FORM_MESSAGE);
+        console.error(err);
+      }
+    });
   }
 
 }
--- consultasCaldas-ng/src/app/services/parametros/municipio.service.ts
+++ consultasCaldas-ng/src/app/services/parametros/municipio.service.ts
@@ -10,20 +10,25 @@ import { MunicipioModel } from 'src/app/models/parametros/municipio.model';
 export class MunicipioService {
   url: string = ConfigurationData.BUSSINESS_MS_URL;
 
-  constructor(private http: HttpClient) { 
-  }
+  constructor(private http: HttpClient) { }
+
   GetRecordList(): Observable<MunicipioModel[]> {
-    return this.http.get<MunicipioModel[]>(`${this.url}/municipios`)
+    return this.http.get<MunicipioModel[]>(`${this.url}/municipios`);
+  }
+
+  SearchRecord(id: number): Observable<MunicipioModel> {
+    return this.http.get<MunicipioModel>(`${this.url}/municipios/${id}`);
   }
 
   SaveRecord(data: MunicipioModel): Observable<MunicipioModel> {
-    return this.http.post<MunicipioModel>(`${this.url}/municipios`,
-      {
-        Nombre: data.Nombre
-      },
-      {
-        // Additional HTTP options can be specified here if needed
-      }
-    );
+    return this.http.post<MunicipioModel>(`${this.url}/municipios`, {
+      Nombre: data.Nombre
+    });
+  }
+
+  EditRecord(data: MunicipioModel): Observable<MunicipioModel> {
+    return this.http.put<MunicipioModel>(`${this.url}/municipios/${data.id}`, {
+      Nombre: data.Nombre
+    });
   }
 }
